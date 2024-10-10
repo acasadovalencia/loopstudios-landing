9985f3b583958ada4300c0db0b484ab5e1b14f0f
Antonio Casado Valencia <acasadovalencia.dev@gmail.com>
2024-10-10 20:40:40 +0200

    fixing header js

--- js/index.js
+++ js/index.js
@@ -3,9 +3,13 @@
 const nav = document.querySelector('.header__nav')
 const openNavBtn = document.querySelector('.menu__svg')
 const closeNavBtn = document.querySelector('.close__svg')
+const header = document.querySelector('.header')
 
 const openNav = () =>{
     nav.classList.toggle('isActive')
+    if(nav.classList.contains('isActive')){
+        header.classList.add('isFixed')
+    }
 }
 
 const closeNav = () =>{
